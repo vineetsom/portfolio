cedb33955b7049e05805855f1abacbb3aa3961b0
Devika <devikavid@gmail.com>
2024-11-27 00:59:55 -0500

    Update components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     <main className="min-h-screen">
       <Parallax />
       <About />
-      <SkillsParallax />
+      {/* <SkillsParallax /> */}
       <AllProjects />
       <Experience />
       <Certifications />
