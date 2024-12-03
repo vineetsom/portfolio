7760da095cefd1b496efdf1633bcce59541546ba
Devika Rajesh Shendkar <55709833+devika7300@users.noreply.github.com>
2024-12-02 21:16:14 -0500

    Update imageLoader.ts

--- src/lib/imageLoader.ts
+++ src/lib/imageLoader.ts
@@ -1,6 +1,6 @@
 export default function imageLoader({ src }: { src: string }) {
-  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
+  // const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
   // Remove any leading slash to avoid double slashes
   const cleanSrc = src.startsWith('/') ? src.slice(1) : src;
-  return `${basePath}/${cleanSrc}`;
+  return `/${cleanSrc}`;
 }
